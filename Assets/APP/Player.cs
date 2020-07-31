fe0c3958dba17383ff6c0645d873733d49d271b4
eric111333 <67904763+eric111333@users.noreply.github.com>
2020-07-31 10:29:50 +0800

    API練習

---
+++ Assets/APP/Player.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// 基礎屬性
+    /// </summary>
+    #region
+    //攻擊力=10 
+    public static float ATK = 10;
+    //防禦力=10
+    public static float DEF = 10;
+    //血量=100
+    public static float HP = 100;
+    //魔力=100
+    public static float MP = 100;
+    //火球術
+    public static string 火球術;
+    //技能一=10
+    public static int Skill_1sp = 10;
+    //治癒術
+    public static string 治癒術;
+    //技能二=30
+    public static int Skill_2sp = 30;
+    //是否死亡=否
+    public static bool Die = false;
+    //速度=50
+    public static float speed = 50;
+
+    #endregion
+
+    /// <summary>
+    /// 基礎設定
+    /// </summary>
+    #region
+    //移動
+    void Move()
+    {
+
+    }
+    //技能1
+    void Skill_1()
+    {
+
+    }
+    //技能2
+    void Skill_2()
+    {
+
+    }
+    //受傷
+    void Hurt()
+    {
+
+    }
+    //死亡
+    void Dead()
+    {
+
+    }
+    //攻擊
+    void Attack()
+    {
+
+    }
+
+    #endregion
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
